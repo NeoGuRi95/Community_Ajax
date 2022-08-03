37b53e108dbd6dd616eaa4608d6933e77afcd961
NeoGuRi95 <jea5158@gmail.com>
2022-08-03 22:14:52 +0900

    delete 추가

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -112,5 +112,10 @@ public class DispatchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         doGet(req, resp);
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
+    }
 }
 
